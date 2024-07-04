c8a225a34494b9396b3a2397ea1b6e3f234b02cc
David Ohimai <daveohimai@gmail.com>
2024-07-04 21:27:00 +0100

    fixed bug

---
+++ task_application/lib/controllers/task_controller.dart
@@ -0,0 +1,37 @@
+import 'package:get/get.dart';
+
+import '../models/task.dart';
+// import 'package:task_application/models/task.dart';
+// import 'package:taskapp_v2/models/task.dart';
+
+
+class TaskController extends GetxController {
+  var tasks = <Task>[].obs;
+
+  void addTask(String title, String description) {
+    tasks.add(Task(
+      title: title,
+      description: description,
+      creationDate: DateTime.now(),
+    ));
+  }
+
+  void updateTask(int index, String title, String description) {
+    var task = tasks[index];
+    task.title = title;
+    task.description = description;
+    tasks[index] = task;
+  }
+
+  void deleteTask(int index) {
+    tasks.removeAt(index);
+  }
+
+  void toggleTaskStatus(int index) {
+    var task = tasks[index];
+    task.isCompleted = !task.isCompleted;
+    tasks[index] = task;
+  }
+
+  int get completedTasks => tasks.where((task) => task.isCompleted).length;
+}
